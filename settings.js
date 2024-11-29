2dea36938f532b9362c42c9bc9418029a9bc8233
Taebearxd <149920040+Taebearxd@users.noreply.github.com>
2024-11-29 21:20:34 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
-  packname: 'My Sticker Pack',
-  author: 'My Bot',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '917023951514', //Your number
+  packname: '𝐃𝐎𝐍𝐓 𝐂𝐎𝐏𝐘 𝐌𝐀𝐇 𝐅𝐋𝐎𝐖 ‼️',
+  author: '⎯⎯ּ︭   🧸̼᪲ 𝐍𝐎𝐈𝐑  ❜❜ ᪶',
+  botName: "NOIR-BOT",
+  botOwner: 'NOIR', // Your name
+  ownerNumber: '917439317360', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
